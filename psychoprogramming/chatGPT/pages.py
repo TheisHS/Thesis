c49a332276897202b3243abf50b0417b78db4943
Nadja Brix Koch <nadjabrixkoch@hotmail.com>
2025-02-28 15:02:12 +0100

    oTree(tasks): setup task pages

--- psychoprogramming/chatGPT/pages.py
+++ psychoprogramming/chatGPT/pages.py
@@ -5,13 +5,29 @@ class Instructions(Page):
     timeout_seconds = 5*60
     template_name = 'chatGPT/templates/Instructions.html'
 
-class TaskDescription1(Page):
+class TaskDescription2(Page):
     timeout_seconds = 3*60
-    template_name = 'chatGPT/templates/TaskDescription1.html'
+    template_name = 'chatGPT/templates/TaskDescriptionPage.html'
 
     @staticmethod
     def vars_for_template(player):
         return dict(
+            id = 2,
+            useAI = False,
+        )
+
+    @staticmethod
+    def before_next_page(player, timeout_happened):
+        requests.get('http://host.docker.internal:5000/setup-exp2')
+
+class TaskDescription1(Page):
+    timeout_seconds = 3*60
+    template_name = 'chatGPT/templates/TaskDescriptionPage.html'
+
+    @staticmethod
+    def vars_for_template(player):
+        return dict(
+            id = 1,
             useAI = False,
         )
 
@@ -27,12 +43,29 @@ class Task1(Page):
     @staticmethod
     def vars_for_template(player):
         return dict(
-            useAI = False,
+            useAI = True,
+            id = 1,
         )
 
     @staticmethod
     def before_next_page(player, timeout_happened):
         requests.get('http://host.docker.internal:5000/takedown-exp1')
+
+class Task2(Page):
+    """The main developer task with a split-screen setup"""
+    timeout_seconds = 20*60
+    template_name = 'chatGPT/templates/Task.html'
+    
+    @staticmethod
+    def vars_for_template(player):
+        return dict(
+            useAI = True,
+            id = 2,
+        )
+
+    @staticmethod
+    def before_next_page(player, timeout_happened):
+        requests.get('http://host.docker.internal:5000/takedown-exp2')
 
 class Break1(Page):
     """Break screen before task description"""
@@ -50,4 +83,4 @@ class Survey(Page):
     form_fields = ['feedback']
     template_name = 'chatGPT/templates/Survey.html'
 
-page_sequence = [Instructions, Break1, TaskDescription1, Task1, Break2, Survey]
+page_sequence = [Instructions, Break1, TaskDescription1, Task1, Break2, TaskDescription2, Task2, Survey]
