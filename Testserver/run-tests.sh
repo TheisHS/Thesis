f0d10c97413b86fd9f4cf973cbfb12960435fd4c
Nadja Brix Koch <nadjabrixkoch@hotmail.com>
2025-03-05 11:38:23 +0100

    code-server: replace bind mount with volume

--- Testserver/run-tests.sh
+++ Testserver/run-tests.sh
@@ -3,6 +3,11 @@
 rm -rf /testing/task/*
 cp /config/workspace/* /testing/task
 
+cd /testing/task
+for f in *.java ; 
+do sed -i '1ipackage task;' $f ; 
+done
+
 cd /testing
 javac /testing/task/*.java
 javac /testing/tests/*.java
--- Testserver/setup-exp.sh
+++ Testserver/setup-exp.sh
@@ -1,9 +1,16 @@
 #!/bin/bash
 
-rm -rf /config/results/exp$1
+rm -rf /results/exp$1
 
 mkdir -p /testing/task
 mkdir -p /testing/tests
+mkdir -p /config/workspace
 
 cp /templates/exp$1/task/* /config/workspace
 cp /templates/exp$1/tests/* /testing/tests
+
+cd /config/workspace
+
+for f in *.java ; 
+do tail -n +2 "$f" > "$f.tmp" && mv "$f.tmp" "$f" ; 
+done
